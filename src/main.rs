7d1684ec3a6b12ee7cfbc0989f9d3d9467085a4d
tropicbliss <tropicbliss@protonmail.com>
2022-03-08 15:33:49 +0800

    E

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,12 @@ use std::io::{self, Result, Write};
 #[derive(Parser)]
 #[clap(author, version, about)]
 /// Feedback character legend:
-///
 /// Initial: 0
-///
 /// Gray: 1
-///
 /// Yellow: 2
-///
 /// Green: 3
 ///
-///
 /// Example:
-///
 /// -----:00000,arose:31112,amend:31211
 pub struct Args {
     /// Play state
