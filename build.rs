c28635dea4462a303ad54f48f01c1e9876e1ff6d
tropicbliss <tropicbliss@protonmail.com>
2022-03-11 19:02:54 +0800

    E

--- build.rs
+++ build.rs
@@ -4,20 +4,11 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::Path;
 
-const DICTIONARY_FILE: &str = "dictionary.txt";
-
 fn main() {
-    let file =
-        read_to_string(DICTIONARY_FILE).expect(&format!("Unable to open {}", DICTIONARY_FILE));
+    let file = read_to_string("dictionary.txt").unwrap();
     let word_data: Vec<_> = file
         .lines()
-        .map(|line| {
-            let (word, count) = line
-                .split_once(' ')
-                .expect("every line is word + space + frequency");
-            count.parse::<usize>().expect("every count is a number");
-            (word, count)
-        })
+        .map(|line| line.split_once(' ').unwrap())
         .collect();
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join("dictionary.rs");
     let mut file = BufWriter::new(File::create(&path).unwrap());
